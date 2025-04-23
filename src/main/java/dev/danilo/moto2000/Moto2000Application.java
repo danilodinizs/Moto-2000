fcf6841be3fe9e52f38387d706088bce22bd5924
danilodinizs <danilodiniz93@yahoo.com>
2025-04-23 16:57:08 -0300

    Initial commit

---
+++ src/main/java/dev/danilo/moto2000/Moto2000Application.java
@@ -0,0 +1,13 @@
+package dev.danilo.moto2000;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Moto2000Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Moto2000Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/danilo/moto2000/Moto2000ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.danilo.moto2000;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Moto2000ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
