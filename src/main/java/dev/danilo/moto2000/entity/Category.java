47685348ac16c92fee1e007850d536fec0bdca85
danilodinizs <danilodiniz93@yahoo.com>
2025-04-24 16:38:16 -0300

    refactor: makes some imports

--- src/main/java/dev/danilo/moto2000/entity/Category.java
+++ src/main/java/dev/danilo/moto2000/entity/Category.java
@@ -7,7 +7,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.UUID;
+import java.util.*;
 
 @Entity
 @Data
--- src/main/java/dev/danilo/moto2000/entity/Client.java
+++ src/main/java/dev/danilo/moto2000/entity/Client.java
@@ -8,7 +8,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-import java.util.UUID;
+import java.util.*;
 
 @Entity
 @Data
