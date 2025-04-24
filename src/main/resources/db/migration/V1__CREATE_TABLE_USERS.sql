bb33a328e94a79e51bd1f87825758d93865e3c76
danilodinizs <danilodiniz93@yahoo.com>
2025-04-24 11:04:00 -0300

    feat: creates users table by Flyway

---
+++ src/main/resources/db/migration/V1__CREATE_TABLE_USERS.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(100) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(20) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
