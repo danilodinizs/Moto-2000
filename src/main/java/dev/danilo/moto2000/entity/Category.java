61b0eaf283af1bf77a141ab4a82a343a80a5c2db
danilodinizs <danilodiniz93@yahoo.com>
2025-04-24 11:44:04 -0300

    feat: creates category entity class

---
+++ src/main/java/dev/danilo/moto2000/entity/Category.java
@@ -0,0 +1,30 @@
+package dev.danilo.moto2000.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "category_tb")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @NotBlank(message = "O nome é obrigatório")
+    @Column(unique = true)
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
+}
