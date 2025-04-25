f5314c5ca365c5ccba1a9052b21741689d8678ea
danilodinizs <danilodiniz93@yahoo.com>
2025-04-25 12:02:24 -0300

    feat: implements findByUsername method

--- src/main/java/dev/danilo/moto2000/repository/UserRepository.java
+++ src/main/java/dev/danilo/moto2000/repository/UserRepository.java
@@ -2,11 +2,12 @@ package dev.danilo.moto2000.repository;
 
 import dev.danilo.moto2000.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
-    //UserDetails findByUsername(String username);
+    UserDetails findByUsername(String username);
 }
