c405e7f3dda551663931f637a5bf86c3fab26fd8
danilodinizs <danilodiniz93@yahoo.com>
2025-04-28 13:33:49 -0300

    feat: finishes response dto class

--- src/main/java/dev/danilo/moto2000/dto/Response.java
+++ src/main/java/dev/danilo/moto2000/dto/Response.java
@@ -3,10 +3,12 @@ package dev.danilo.moto2000.dto;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import dev.danilo.moto2000.enums.UserRole;
+import lombok.Builder;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record Response(
