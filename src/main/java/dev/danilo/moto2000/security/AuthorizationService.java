5f2e53a1a9dcf1c38390d32687fcb2b0ee6eb846
danilodinizs <danilodiniz93@yahoo.com>
2025-04-28 13:58:17 -0300

    feat: creates authorization service and implements user details service

---
+++ src/main/java/dev/danilo/moto2000/security/AuthorizationService.java
@@ -0,0 +1,27 @@
+package dev.danilo.moto2000.security;
+
+import dev.danilo.moto2000.entity.User;
+import dev.danilo.moto2000.exceptions.NotFoundException;
+import dev.danilo.moto2000.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthorizationService implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = repository.findByUsername(username)
+                .orElseThrow(() -> new NotFoundException("Username not found"));
+
+        return AuthUser.builder()
+                .user(user)
+                .build();
+    }
+}
