97efe79c904dceff5877bb7356f0adbb5327988a
danilodinizs <danilodiniz93@yahoo.com>
2025-04-28 15:17:36 -0300

    feat: finalizes spring security config

--- src/main/java/dev/danilo/moto2000/security/AuthService.java
+++ src/main/java/dev/danilo/moto2000/security/AuthService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class AuthorizationService implements UserDetailsService {
+public class AuthService implements UserDetailsService {
 
     private final UserRepository repository;
 
---
+++ src/main/java/dev/danilo/moto2000/security/JwtUtils.java
@@ -0,0 +1,62 @@
+package dev.danilo.moto2000.security;
+
+import com.auth0.jwt.JWT;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+@Slf4j
+public class JwtUtils {
+
+    private SecretKey key;
+
+    @Value("${jwt.expiration")
+    private Long jwtExpiration;
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @PostConstruct
+    private void init() {
+        byte[] keyByte = secretKey.getBytes(StandardCharsets.UTF_8);
+        this.key = new SecretKeySpec(keyByte, "HmacSHA256");
+    }
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .signWith(key)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return extractClaims(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaims(String token, Function<Claims, T> claimsTFunction) {
+        return claimsTFunction.apply(Jwts.parser().verifyWith(key).build().parseSignedClaims(token).getPayload());
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    public boolean isTokenExpired(String token){
+        return extractClaims(token, Claims::getExpiration).before(new Date());
+    }
+
+}
