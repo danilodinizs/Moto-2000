f2a28c373835dd9d2945558120e775b472f7ba50
danilodinizs <danilodiniz93@yahoo.com>
2025-04-28 13:34:11 -0300

    feat: create all excepetions and global exception handler

---
+++ src/main/java/dev/danilo/moto2000/exceptions/NameValueRequiredException.java
@@ -0,0 +1,8 @@
+package dev.danilo.moto2000.exceptions;
+
+public class NameValueRequiredException extends RuntimeException {
+
+    public NameValueRequiredException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/dev/danilo/moto2000/exceptions/NotFoundException.java
@@ -0,0 +1,8 @@
+package dev.danilo.moto2000.exceptions;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
