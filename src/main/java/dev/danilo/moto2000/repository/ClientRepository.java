e135a3a9abfa137ccc60d39d1c1f327863b9d10d
danilodinizs <danilodiniz93@yahoo.com>
2025-05-02 14:09:24 -0300

    feat: adds find by: name, cpf, email and phone number query to repository

--- src/main/java/dev/danilo/moto2000/repository/ClientRepository.java
+++ src/main/java/dev/danilo/moto2000/repository/ClientRepository.java
@@ -1,11 +1,17 @@
 package dev.danilo.moto2000.repository;
 
+import dev.danilo.moto2000.dto.ClientDTO;
 import dev.danilo.moto2000.entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, UUID> {
+    Optional<ClientDTO> findByName(String name);
+    Optional<ClientDTO> findByCpf(String cpf);
+    Optional<ClientDTO> findByEmail(String email);
+    Optional<ClientDTO> findByPhoneNumber(String phoneNumber);
 }
