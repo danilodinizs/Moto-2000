13693e2d928dd32c647fe59e62b9300d0b8c1346
danilodinizs <danilodiniz93@yahoo.com>
2025-05-06 15:29:22 -0300

    feat: creates supplier service interface

---
+++ src/main/java/dev/danilo/moto2000/service/SupplierService.java
@@ -0,0 +1,14 @@
+package dev.danilo.moto2000.service;
+
+import dev.danilo.moto2000.dto.Response;
+import dev.danilo.moto2000.dto.SupplierDTO;
+
+import java.util.UUID;
+
+public interface SupplierService {
+    Response addSupplier(SupplierDTO supplierDTO);
+    Response updateSupplier(UUID id, SupplierDTO supplierDTO);
+    Response getAllSuppliers();
+    Response getSupplierById(UUID id);
+    Response deleteSupplier(UUID uuid);
+}
