c4b73ad5bcd0dd11253f00220f1eff043bc85cb3
danilodinizs <danilodiniz93@yahoo.com>
2025-05-07 14:24:50 -0300

    feat: creates Service Order entity

---
+++ src/main/java/dev/danilo/moto2000/entity/ServiceOrder.java
@@ -0,0 +1,47 @@
+package dev.danilo.moto2000.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "service_order_tb")
+public class ServiceOrder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private String name;
+
+    private String description;
+
+    private String duration;
+
+    @Column(name = "labor_cost")
+    private BigDecimal laborCost;
+
+    @ManyToMany
+    @JoinTable(
+            name = "service_order_products",
+            joinColumns = @JoinColumn(name = "service_order_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id")
+    )
+    private List<Product> products;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
