0ab992b8de9ecc1212bfd23f62beb5fab649cabc
danilodinizs <danilodiniz93@yahoo.com>
2025-05-07 13:30:38 -0300

    feat: includes more types for this enum

--- src/main/java/dev/danilo/moto2000/enums/MotorcycleType.java
+++ src/main/java/dev/danilo/moto2000/enums/MotorcycleType.java
@@ -1,5 +1,14 @@
 package dev.danilo.moto2000.enums;
 
 public enum MotorcycleType {
-    SCOOTER, MANUAL
+    ESPORTIVA,
+    STREET,
+    CUSTOM,
+    TURISMO,
+    OFF_ROAD,
+    AVENTURA,
+    SCOOTER,
+    CAFE_RACER,
+    CHOPPER,
+    ELETRICA
 }
