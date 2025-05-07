db1fa373a673b2a1555c03c364842a6f3cea153b
danilodinizs <danilodiniz93@yahoo.com>
2025-05-07 14:34:58 -0300

    feat: creates service order dto

---
+++ src/main/java/dev/danilo/moto2000/dto/ServiceOrderDTO.java
@@ -0,0 +1,38 @@
+package dev.danilo.moto2000.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ServiceOrderDTO {
+
+    private UUID id;
+
+    @NotBlank(message = "O nome é obrigatório")
+    private String name;
+
+    private String description;
+
+    private String duration;
+
+    @Positive(message = "O valor da mão de obra deve ser positivo")
+    private BigDecimal laborCost;
+
+    private LocalDateTime createdAt;
+
+    private List<UUID> productsIds;
+}
