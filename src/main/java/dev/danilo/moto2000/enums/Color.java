9fed25cb6bc49eef31aa33aaefefb8bdaa295d42
danilodinizs <danilodiniz93@yahoo.com>
2025-05-07 13:29:28 -0300

    feat: creates a color enum

---
+++ src/main/java/dev/danilo/moto2000/enums/Color.java
@@ -0,0 +1,22 @@
+package dev.danilo.moto2000.enums;
+
+public enum Color {
+    PRETO,
+    BRANCO,
+    VERMELHO,
+    AZUL,
+    CINZA,
+    PRATA,
+    VERDE,
+    AMARELO,
+    LARANJA,
+    MARROM,
+    ROSA,
+    ROXO,
+    DOURADO,
+    BEGE,
+    VINHO,
+    AZUL_METALICO,
+    CINZA_FOSCO,
+    PRETO_FOSCO;
+}
