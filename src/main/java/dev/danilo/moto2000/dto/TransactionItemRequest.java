2679a7097ab24a4edfa7e96397e342ff6b07b9d7
danilodinizs <danilodiniz93@yahoo.com>
2025-05-09 17:21:02 -0300

    feat: creates a new dto to add products with quantity into a transaction

---
+++ src/main/java/dev/danilo/moto2000/dto/TransactionItemRequest.java
@@ -0,0 +1,26 @@
+package dev.danilo.moto2000.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TransactionItemRequest {
+
+        private UUID productId;
+
+        private Integer quantity;
+
+}
