d76723109244ef6a8b0dc62196f21359eed0bcce
danilodinizs <danilodiniz93@yahoo.com>
2025-05-13 11:17:03 -0300

    feat: creates transaction controller

---
+++ src/main/java/dev/danilo/moto2000/controller/TransactionController.java
@@ -0,0 +1,25 @@
+package dev.danilo.moto2000.controller;
+
+import dev.danilo.moto2000.dto.Response;
+import dev.danilo.moto2000.dto.TransactionRequest;
+import dev.danilo.moto2000.service.TransactionService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/api/transactions")
+@RequiredArgsConstructor
+public class TransactionController {
+
+    private final TransactionService service;
+
+    @PostMapping("/purchase")
+    public ResponseEntity<Response> restockInventory(@RequestBody @Valid TransactionRequest request) {
+        return ResponseEntity.ok(service.restockInventory(request));
+    }
+}
