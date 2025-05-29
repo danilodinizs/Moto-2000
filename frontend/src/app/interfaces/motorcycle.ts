d5c1edd0a917c614e35fe1fe127378b746b262d9
danilodinizs <danilodiniz93@yahoo.com>
2025-05-29 11:32:19 -0300

    feat: creates motorcycle interface

---
+++ frontend/src/app/interfaces/motorcycle.ts
@@ -0,0 +1,14 @@
+import { Color } from "../enums/color";
+import { MotorcycleType } from "../enums/motorcycle-type";
+
+export interface Motorcycle {
+  id?: string;
+  licensePlate: string;
+  model: string;
+  year: number;
+  color: Color;
+  motorcycleType: MotorcycleType;
+  clientId: string;
+  createdAt: string;
+  updatedAt?: string;
+}
