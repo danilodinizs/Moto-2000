55812843d19087e8fa32d025f6f7322b0b300092
danilodinizs <danilodiniz93@yahoo.com>
2025-05-30 14:49:32 -0300

    feat: includes sidebar html and logic

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ApiService } from './service/api.service';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { Observable, startWith } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, RouterLink, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'Moto 2000';
+
+  isAuthenticated$!: Observable<boolean>;
+  isAdmin$!: Observable<boolean>;
+
+  constructor(
+    private apiService: ApiService,
+    private router: Router,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  ngOnInit(): void {
+    this.isAuthenticated$ = this.apiService.isAuthenticated();
+    this.isAdmin$ = this.apiService.isAdmin();
+  }
+
+  logout(): void {
+    this.apiService.logout();
+    this.router.navigate(['/login']);
+    this.cdr.detectChanges();
+  }
 }
