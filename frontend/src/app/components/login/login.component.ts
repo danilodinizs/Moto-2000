7aab55f8e281be05fd5b09fede2b691c1c017664
danilodinizs <danilodiniz93@yahoo.com>
2025-06-10 19:02:45 -0300

    feat: implements login component rules

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,11 +1,43 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { ApiService } from '../../service/api.service';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, CommonModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
 
+export class LoginComponent {
+  constructor(private apiService: ApiService, private router: Router) {}
+
+  formData: any = { username: '', password: '' };
+  message: string | null = null;
+
+  async handleLogin() {
+    if (!this.formData.username || !this.formData.password) {
+      this.showMessage("Todos os campos são necessários");
+      return;
+    }
+
+    try {
+      await firstValueFrom(this.apiService.loginUser(this.formData));
+
+      this.router.navigate(["/dashboard"]);
+
+    } catch (error: any) {
+        console.error("Erro no login:", error);
+        this.showMessage(error?.error?.message || "Falha no login. Verifique suas credenciais.");
+    }
+  }
+
+  showMessage(message: string) {
+    this.message = message;
+    setTimeout(() => { this.message = null; }, 4000);
+  }
 }
