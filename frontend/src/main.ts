8b38c515fa0afab4c6a2a18d0997ae6a969b497c
danilodinizs <danilodiniz93@yahoo.com>
2025-06-11 11:00:52 -0300

    feat: includes app initializer to fix f5 issue

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,27 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { APP_INITIALIZER, importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
+import { ApiService } from './app/service/api.service';
+import { firstValueFrom } from 'rxjs';
+import { routes } from './app/app.routes';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+export function initApp(apiService: ApiService) {
+  return () => firstValueFrom(apiService.checkAuthStatus());
+}
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(),
+    provideRouter(routes),
+
+    ApiService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initApp,
+      deps: [ApiService],
+      multi: true
+    }
+  ]
+}).catch(err => console.error(err));
