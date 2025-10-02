cadd25236f926dfc32b9aadcf49808462980e8bd
Danilo Diniz <danilodiniz93@yahoo.com>
2025-10-02 09:31:53 -0300

    feat: implements motorcycle ts rules

--- frontend/src/app/components/motorcycle/motorcycle-list/motorcycle.component.ts
+++ frontend/src/app/components/motorcycle/motorcycle-list/motorcycle.component.ts
@@ -1,11 +1,68 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../../service/api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-motorcycle',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './motorcycle.component.html',
   styleUrl: './motorcycle.component.css'
 })
-export class MotorcycleComponent {
+export class MotorcycleComponent implements OnInit{
+  constructor(private apiService:ApiService, private router:Router) {}
+  motorcycles:any[] = [];
+  message:string = '';
 
+  ngOnInit(): void {
+    this.getMotorcycles();
+  }
+
+  getMotorcycles():void {
+    this.apiService.getAllMotorcycles().subscribe({
+      next:(res:any) => {
+        if (res.status === 200) {
+          this.motorcycles = res.motorcycles;
+        } else {
+          this.showMessage(res.message)
+        }
+      }, 
+      error:(error) => {
+          this.showMessage(error?.error?.message || error?.message || "Não foi possível recuperar as motocicletas: " + error)
+
+      }
+    })
+  }
+
+  // navigate to add motorcycle page
+  navigateToAddMotorcyclePage():void{
+    this.router.navigate([`/add-motorcycle`]);
+  }
+
+  // navigate to add motorcycle page
+  navigateToEditMotorcyclePage(motorcycleId: string):void{
+    this.router.navigate([`/edit-motorcycle/${motorcycleId}`]);
+  }
+
+  // DELETE MOTORCYCLE
+  deleteMotorcycle(motorcycleId: string): void {
+    if (window.confirm("Você tem certeza que quer apagar essa motocicleta?"))
+      this.apiService.deleteMotorcycle(motorcycleId).subscribe({
+      next:(res:any) => {
+        this.showMessage("Motocicleta apagado com sucesso")
+        this.getMotorcycles(); // reload the motorcycle     
+      }, 
+      error:(error) => {
+          this.showMessage(error?.error?.message || error?.message || "Não foi possível apagar a motocicleta: " + error)
+
+      }
+    })
+  }
+
+  showMessage(message: string) {
+    this.message = message;
+    setTimeout(() => {
+      this.message = '';
+    }, 4000)
+  }
 }
